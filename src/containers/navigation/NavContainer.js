d75f1387c5f456c44e626619e9ccebdc5eeed2ee
JTSwisher <jefftswisher@gmail.com>
2020-08-31 15:55:31 -0700

    add NavBar and NavMenuToggle components to NavContainer

--- src/containers/navigation/NavContainer.js
+++ src/containers/navigation/NavContainer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import NavBar from './components/NavBar';
+import NavMenuToggle from './components/NavMenuToggle';
+
 class NavContainer extends Component {
 
     render() {
         return(
-            <div className="nav-bar">
-                
+            <div>
+                <NavMenuToggle />
+                <NavBar />
             </div>
         )
     }
---
+++ src/containers/navigation/components/NavMenuToggle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NavMenuToggle() {
+    return (
+        <div className="nav-menu-toggle">
+
+        </div>
+    )
+}
