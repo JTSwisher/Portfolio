34b8536e070c4d7b019ca0f2a6ad8480f6371f23
JTSwisher <jefftswisher@gmail.com>
2020-08-31 15:41:34 -0700

    add NavContainer class component for nav menu

---
+++ src/containers/navigation/NavContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NavContainer extends Component {
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default NavContainer;
