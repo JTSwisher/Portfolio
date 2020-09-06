cc8cfb63a329a5249381622ef07f5a213cf6368b
JTSwisher <jefftswisher@gmail.com>
2020-09-06 09:29:22 -0700

    add ProjectNav to ProjectContainer, add state & callback to project conatiner to handle selected project state

--- src/containers/projects/ProjectContainer.js
+++ src/containers/projects/ProjectContainer.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
-import NavContainer from '../navigation/NavContainer'
+import NavContainer from '../navigation/NavContainer';
+import ProjectNav from './components/ProjectNav'
+
 
 export default class ProjectContainer extends Component{
+
+    state = {
+        project: 'DailyDash',
+    }
+
+    handleProjectSelect = (e) => {
+        this.setState({
+            project: ''
+        })
+    }
 
     render() {
         return (
             <div className="project-container">
                 <NavContainer />
-                Hey
+                <ProjectNav projectSelect={this.handleProjectSelect}/>
             </div>
         )
     }
---
+++ src/containers/projects/components/ProjectNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ProjectNav() {
+
+    return (
+        <nav className="project-nav">
+            <div>DailyDash</div>
+            <div>GifBook</div>
+            <div>CocktailKeeper</div>
+        </nav>
+    )
+}
