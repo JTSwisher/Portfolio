4b452dfea3b5b01fb89ce05646b9c060a2499438
JTSwisher <jefftswisher@gmail.com>
2020-09-17 20:50:26 -0700

    adjust HomeContainer RightSide styling

--- src/containers/home/components/RightSide.js
+++ src/containers/home/components/RightSide.js
@@ -5,7 +5,7 @@ function RightSide() {
         <div className="home-right-side">
             <div className="greeting-right">
                 <p id="title-greeting-right">I'm a Full-Stack Software Engineer in Scottsdale, AZ</p>
-                <p id="subtitle-greeting-right">I want to create things that <strong><u id="greeting-right-line">Enhance Lives</u></strong></p>
+                <p id="subtitle-greeting-right">I want to create things that <strong><b className="greeting-line">Enhance Lives</b></strong></p>
             </div>
         </div>
     )
